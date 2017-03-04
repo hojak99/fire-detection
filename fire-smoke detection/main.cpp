0da26195b2ae2daab5c11499d149c62b7a566c4a
hojak99 <hojak99@gmail.com>
2017-03-04 22:52:57 +0900

    화재 영상 열고 재생

---
+++ fire-smoke detection/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <opencv2\opencv.hpp>
+
+int main()
+{
+	cv::VideoCapture video("test_fire_2.mp4");
+
+	if (!video.isOpened()) {
+		std::cout << "Can not open video" << std::endl;
+		return 0;
+	}
+
+	cv::Mat frame;
+	cv::namedWindow("FireVideo", 1);
+
+	while (1) {
+		video >> frame;
+
+		cv::imshow("FireVideo", frame);
+
+		if (cv::waitKey(30) == 27) {
+			break;
+		}
+	}
+
+	cv::destroyAllWindows();
+	frame.release();
+
+	return 0;
+}
